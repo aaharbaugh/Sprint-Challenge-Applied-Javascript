9add1d5089a7f173243d1880d73ec4e23980a57b
aaharbaugh <aaronmharbaugh@gmail.com>
2019-02-09 12:19:43 -0600

    finished mvp and stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,53 @@
 class Carousel {
+    constructor(carouselElement){
+        this.carousel = carouselElement;
+        this.leftButton = this.carousel.querySelector('.left-button');
+        this.rightButton = this.carousel.querySelector('.right-button');
+        this.images = this.carousel.querySelectorAll('img')
 
+        this.counter = 0;
+
+        this.images[this.counter].style.display = "flex"
+        this.rightButton.addEventListener('click', (e) => {
+            this.rightClick()
+        })
+        this.leftButton.addEventListener('click', (e) => {
+            this.leftClick()
+        })
+
+
+    }
+
+    rightClick(){
+        this.counter++
+        if(this.counter === 4){
+            this.counter = 0;
+        }
+        this.images.forEach(image => {
+            image.style.display = "none";
+        })
+        this.images[this.counter].style.display = "flex"
+    }
+
+    leftClick(){
+        this.counter--
+        if(this.counter < 0){
+            this.counter = 3;
+        }
+        this.images.forEach(image => {
+            image.style.display = "none";
+        })
+        this.images[this.counter].style.display = "flex"
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+
+let newCarousel = new Carousel(carousel);
+
+setInterval(newCarousel.rightClick.bind(newCarousel), 5000);
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
